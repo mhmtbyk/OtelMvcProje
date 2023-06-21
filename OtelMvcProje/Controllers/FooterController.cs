cdc8392948de20c842916f6d131b0a5239bd4902
Mehmet Bayakı <mhmt_byk99@hotmail.com>
2023-06-21 20:54:11 +0300

    1) Footer, partial ile oluşturuldu. 2) Fotter bölümüne sosyal medya ikonları eklendi. 3)_OtelVitrin layoutunda partiallar çağırıldı. #Web Sitesi panomuzda 8. Alt Bilgiler kartı tamamlandı.

---
+++ OtelMvcProje/Controllers/FooterController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OtelMvcProje.Controllers
+{
+    public class FooterController : Controller
+    {
+        // GET: Footer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSosyalMedya()
+        {
+            return PartialView();
+        }
+    }
+}
