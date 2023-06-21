c6c34b218db6b109ef05a91bb07fcaa67c18b49f
Mevlüt Dızman <mdizman124@gmail.com>
2023-06-21 21:43:36 +0300

    1) Hakkımızda sayfasının tasarımı yapıldı. 2)Hakkımızda sayfasının içeriği girildi. 3)Bazı yazılar veri tabanından çekildi. #Web Sitesi panomuzda 2. Hakkımızda kartı tamamlandı.

---
+++ OtelMvcProje/Controllers/HakkimizdaController.cs
@@ -0,0 +1,20 @@
+﻿using OtelMvcProje.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OtelMvcProje.Controllers
+{
+    public class HakkimizdaController : Controller
+    {
+        // GET: Hakkimizda
+        DbOtelEntities db = new DbOtelEntities();
+        public ActionResult Index()
+        {
+            var veriler = db.TblHakkimda.ToList();
+            return View(veriler);
+        }
+    }
+}
